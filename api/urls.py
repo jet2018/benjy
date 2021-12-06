7dac5fb8a7f55835b603579e6e5d7f60d86d6a6c
jet2018 <ezrajet9@gmail.com>
2021-12-07 00:23:55 +0300

    added cool charts

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('json/', views.data_to_json, name='json'),
 ]
--- api/views.py
+++ api/views.py
@@ -24,14 +24,15 @@ class HomePageView(LoginRequiredMixin, TemplateView):
             'https://devicemonitor-2e4ba-default-rtdb.firebaseio.com/users.json')
         data = get_data.json()
         eachUser = []
+        fb = []
+        wp = []
+        tw = []
+        sn = []
+        inst = []
+        reddit = []
         for a in data:
             all_data = data[a]
-            fb = []
-            wp = []
-            tw = []
-            sn = []
-            inst = []
-            reddit = []
+
             for b in all_data["usagedata"]:
                 if b["faceboook"]:
                     fb.append(round(int(b["faceboook"])/60000))
@@ -45,32 +46,70 @@ class HomePageView(LoginRequiredMixin, TemplateView):
                     inst.append(round(int(b["instagram"])/60000))
                 if b['reddit']:
                     reddit.append(round(int(b['reddit'])/60000))
-
-            # getting sums
-            fb_sum = sum(fb)
-            wp_sum = sum(wp)
-            tw_sum = sum(tw)
-            sn_sum = sum(sn)
-            inst_sum = sum(inst)
-            reddit_sum = sum(reddit)
-            usage = []
-            # getting the highest usage
-            if fb_sum > wp_sum and fb_sum > tw_sum and fb_sum > sn_sum and fb_sum > inst_sum and fb_sum > reddit_sum:
-                usage.append({"usage": fb_sum, "device": "facebook"})
-            elif wp_sum > fb_sum and wp_sum > tw_sum and wp_sum > sn_sum and wp_sum > inst_sum and wp_sum > reddit_sum:
-                usage.append({"usage": wp_sum, "device": "whatsapp"})
-            elif tw_sum > fb_sum and tw_sum > wp_sum and tw_sum > sn_sum and tw_sum > inst_sum and tw_sum > reddit_sum:
-                usage.append({"usage": tw_sum, "device": "twitter"})
-            elif sn_sum > fb_sum and sn_sum > wp_sum and sn_sum > tw_sum and sn_sum > inst_sum and sn_sum > reddit_sum:
-                usage.append({"usage": sn_sum, "device": "snapchat"})
-            elif inst_sum > fb_sum and inst_sum > wp_sum and inst_sum > tw_sum and inst_sum > sn_sum and inst_sum > reddit_sum:
-                usage.append({"usage": inst_sum, "device": "instagram"})
-            elif reddit_sum > fb_sum and reddit_sum > wp_sum and reddit_sum > tw_sum and reddit_sum > sn_sum and reddit_sum > inst_sum:
-                usage.append({"usage": reddit_sum, "device": "reddit"})
-
             eachUser.append(
                 {"name": all_data["fullname"], "username": all_data["username"], "fb": sum(fb), "wp": sum(wp), "tw": sum(tw), "reddit": sum(reddit), "inst": sum(inst), 'sn': sum(sn)})
+
+        # getting sums
+        fb_sum = sum(fb)
+        wp_sum = sum(wp)
+        tw_sum = sum(tw)
+        sn_sum = sum(sn)
+        inst_sum = sum(inst)
+        reddit_sum = sum(reddit)
+        usage = []
+        # getting the highest usage
+        if fb_sum > wp_sum and fb_sum > tw_sum and fb_sum > sn_sum and fb_sum > inst_sum and fb_sum > reddit_sum:
+            usage.append({"usage": fb_sum, "device": "facebook"})
+        elif wp_sum > fb_sum and wp_sum > tw_sum and wp_sum > sn_sum and wp_sum > inst_sum and wp_sum > reddit_sum:
+            usage.append({"usage": wp_sum, "device": "whatsapp"})
+        elif tw_sum > fb_sum and tw_sum > wp_sum and tw_sum > sn_sum and tw_sum > inst_sum and tw_sum > reddit_sum:
+            usage.append({"usage": tw_sum, "device": "twitter"})
+        elif sn_sum > fb_sum and sn_sum > wp_sum and sn_sum > tw_sum and sn_sum > inst_sum and sn_sum > reddit_sum:
+            usage.append({"usage": sn_sum, "device": "snapchat"})
+        elif inst_sum > fb_sum and inst_sum > wp_sum and inst_sum > tw_sum and inst_sum > sn_sum and inst_sum > reddit_sum:
+            usage.append({"usage": inst_sum, "device": "instagram"})
+        elif reddit_sum > fb_sum and reddit_sum > wp_sum and reddit_sum > tw_sum and reddit_sum > sn_sum and reddit_sum > inst_sum:
+            usage.append({"usage": reddit_sum, "device": "reddit"})
 
         context['usages'] = usage
         context['Students'] = eachUser
         return context
+
+
+@api_view(['GET'])
+def data_to_json(request):
+    get_data = requests.get(
+        'https://devicemonitor-2e4ba-default-rtdb.firebaseio.com/users.json')
+    data = get_data.json()
+    fb = []
+    wp = []
+    tw = []
+    sn = []
+    inst = []
+    reddit = []
+    for a in data:
+        all_data = data[a]
+
+        for b in all_data["usagedata"]:
+            if b["faceboook"] and b["faceboook"] != "0":
+                fb.append(round(int(b["faceboook"])/60000))
+            if b["whatsapp"] and b["whatsapp"] != "0":
+                wp.append(round(int(b["whatsapp"])/60000))
+            if b["twitter"] and b["twitter"] != "0":
+                tw.append(round(int(b["twitter"])/60000))
+            if b["snapchat"] and b["snapchat"] != "0":
+                sn.append(round(int(b["snapchat"])/60000))
+            if b["instagram"] and b["instagram"] != "0":
+                inst.append(round(int(b["instagram"])/60000))
+            if b['reddit'] and b['reddit'] != "0":
+                reddit.append(round(int(b['reddit'])/60000))
+    fb_sum = sum(fb)
+    wp_sum = sum(wp)
+    tw_sum = sum(tw)
+    sn_sum = sum(sn)
+    inst_sum = sum(inst)
+    reddit_sum = sum(reddit)
+    usage = []
+    usage.append({"facebook": fb_sum, "whatsapp": wp_sum, "twitter": tw_sum,
+                  "instagram": inst_sum, "reddit": reddit_sum, "snapchat": sn_sum})
+    return JsonResponse(usage, safe=False)
